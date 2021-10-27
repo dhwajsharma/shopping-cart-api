11cff600e353072c96c291d5b22109e832755893
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-10-27 08:34:07 +0530

    Created MongoDB models.

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL)
-.then(()=> console.log("Connected to DB"))
-.catch(err => console.log("Error: " + err)); 
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log("Error: " + err));
 
+app.use(express.json());
+app.use("api/users", userRoute);
 
-app.listen(process.env.PORT || 5000, () =>{
-    console.log('listening on port 5000');
-})
+app.listen(process.env.PORT || 5000, () => {
+  console.log("listening on port 5000");
+});
